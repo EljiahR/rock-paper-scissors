aa1e04477063ce3ce79a03ecfee720adaca3416e
Elijah <kcerhajile@gmail.com>
2023-08-30 18:27:48 -0400

    Added running total for wins and a 'final' win or lose base on the first to 5

--- javascript.js
+++ javascript.js
@@ -23,9 +23,13 @@ function playRound(playerChoice){
     if(playerChoice == computerChoice){
         return "Draw";
     } else if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")){
+        yourScore++;
         return "You Win!";
+        
     } else {
+        computerScore++;
         return "You lose";
+        
     };
 };
 
@@ -50,14 +54,25 @@ function playScissors(){
 
 function addResults(result){
     let newResult = document.createElement('p');
-    newResult.innerText = result;
+    newResult.innerText = `${result} Score: ${yourScore}:${computerScore}`;
     results.appendChild(newResult);
+    if(yourScore == 5 || computerScore == 5){
+        let finalResults = document.createElement('p');
+        if(yourScore > computerScore && yourScore ==5){
+            finalResults.innerText="Final Results: YOU WIN!!!!";
+        }else {
+            finalResults.innerText="Final Results: You lose :(";
+        }
+        results.appendChild(finalResults);
+    }
 };
 
 const rockBtn = document.querySelector('.rock');
 const paperBtn = document.querySelector('.paper');
 const scissorsBtn = document.querySelector('.scissors');
 const results = document.querySelector('.results');
+let yourScore = 0;
+let computerScore = 0;
 
 rockBtn.addEventListener('click', playRock);
 paperBtn.addEventListener('click', playPaper);
